f856aa23d69655ac9f32be3dfc64fcaaaf7cf4c4
prabhat-123 <srv.ale52@gmail.com>
2023-02-21 18:33:54 +0545

    features/pa/modified operations/added two functions; one for finding disjoint elem and one for cretating a freq table

--- operations.py
+++ operations.py
@@ -4,3 +4,20 @@ def find_common_elem(list1, list2):
         if item in list2:
             common_elem.append(item)
     return common_elem
+
+def find_disjoint_elem(list1, list2):
+
+    disjoint_elem = []
+    for item in list1:
+        if item not in list2:
+            disjoint_elem.append(item)
+    return disjoint_elem
+
+def freq_table(list1):
+    table = {}
+    for item in list1:
+        if item not in table:
+            table[item] = 1
+        else:
+            table[item] += 1
+    return table
